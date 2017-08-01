d1541d3156a7d85b3d2c19044ee6b68c5a7b2450
Hernan Tejada <hernan.tejada@gmail.com>
2017-08-01 09:17:20 -0400

    Improved cloze deletion to multiple deletions at one time

--- ClozeCard.js
+++ ClozeCard.js
@@ -11,16 +11,29 @@ ClozeCard.prototype.partial = function() {
 
 	var lowText = this.fullText.toLowerCase();
 	var lowCloze = this.cloze.toLowerCase();
-	var textSwap = this.fullText.replace(this.cloze, "...");
-	console.log(lowText);
-	console.log(lowCloze);
-	console.log(textSwap);
-
-	if (!lowText.includes(lowCloze)) {
-		return (this.cloze + " doesn't appear in " + this.fullText);
-	} else {
-		return (textSwap);
+	var fulltextArray = this.fullText.split(" ");
+	var lowtextArray = lowText.split(" ");
+	var lowclozeArray = lowCloze.split(" ");
+	var finalText = "";
+	var cleanText = "";	
+	
+	for (var i = 0; i < lowtextArray.length; i++) {
+		for (var j = 0; j < lowclozeArray.length; j++) { 
+			if (lowtextArray[i] === lowclozeArray[j]) {
+				fulltextArray[i] = "...";
+			} else if (!lowText.includes(lowclozeArray[j])) {
+				return (lowclozeArray[j] + " doesn't appear in " + this.fullText);
+			}
+		}
 	}
-}
+	
+	finalText = fulltextArray.join(" ");
+	cleanText = finalText.replace("... ...", "...")
+
+	return (cleanText);
+	
+};
 
 module.exports = ClozeCard;
+
+
